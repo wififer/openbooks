d2e8eea6aba69bc3f104652f2b1163dc1e78670c
Fernando Ransanz <uxama50@hotmail.com>
2016-01-04 10:49:52 +0100

    Crear Reachability.swift

---
+++ openbooks/Reachability.swift
@@ -0,0 +1,42 @@
+//
+//  Reachability.swift
+//  Openbboks
+//
+//  Created by wififer on 08/12/15.
+//  Copyright © 2015 wififer. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    
+    // Check if internet connection is available.
+    
+    class func isConnectedToNetwork() -> Bool {
+        
+        var status:Bool = false
+        
+        let url = NSURL(string: "https://google.com")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "HEAD"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
+        request.timeoutInterval = 10.0
+        
+        var response:NSURLResponse?
+        
+        do {
+            let _ = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response) as NSData?
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        if let httpResponse = response as? NSHTTPURLResponse {
+            if httpResponse.statusCode == 200 {
+                status = true
+            }
+        }
+        return status
+    }
+}
